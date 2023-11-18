db436b1db585c1b311a29bcbac0780df6b045f41
Jan Z <jan.znz@hotmail.com>
2023-11-18 20:15:01 +0100

    Periodic funktioniert

--- src/euler_solver.jl
+++ src/euler_solver.jl
@@ -11,7 +11,7 @@ function solve_euler(sys::HyDySys, σ::Float64, a::Float64, t_ende::Float64)
     us      = copy(sys.us)
     ϵs      = copy(sys.ϵs)
     γ       = sys.γ
-    Ordnung = 2
+    ordnung = 2
 
     # Implementiere Formeln zur Lösung nach dem Schema des Opeartor-Splitting
 
@@ -34,7 +34,7 @@ function solve_euler(sys::HyDySys, σ::Float64, a::Float64, t_ende::Float64)
             if critical == 0
                 return 2 * x / 0.000001
             elseif critical != 0
-                return 2 * x / (ρs[j+1]-ρs[j-1])
+                return 2 * x / critical
             end
         else
             return 0
@@ -196,9 +196,9 @@ function solve_euler(sys::HyDySys, σ::Float64, a::Float64, t_ende::Float64)
             ρs = vcat([ ρs[2], ρs[1]], ρs, [ρs[end], ρs[end-1]])
             ϵs = vcat([ ϵs[2], ϵs[1]], ϵs, [ϵs[end], ϵs[end-1]])
         elseif sys.bound_cond == :periodic
-            ρs = vcat(ρs[end-Ordnung+1:end], ρs, ρs[1:Ordnung])
-            us = vcat(us[end-Ordnung+1:end], us, us[1:Ordnung])
-            ϵs = vcat(ϵs[end-Ordnung+1:end], ϵs, ϵs[1:Ordnung])
+            ρs = vcat(ρs[end-ordnung+1:end], ρs, ρs[1:ordnung])
+            us = vcat(us[end-ordnung+1:end], us, us[1:ordnung])
+            ϵs = vcat(ϵs[end-ordnung+1:end], ϵs, ϵs[1:ordnung])
         else
             error("Boundary condition not valid: $(sys.bound_cond)")
             return false
@@ -219,16 +219,16 @@ function solve_euler(sys::HyDySys, σ::Float64, a::Float64, t_ende::Float64)
         # u → u^1
         # ϵ → ϵ^1
         # Berechne zuerst die neue Dichte des Systems:
-        for j in (Ordnung+1):(l + Ordnung - 1)
+        for j in (ordnung+1):(l + ordnung - 1)
             ρs[j] = ρ_new(ρs_copy, us_copy, j, Δt, Δx)
 
         end
 
         # Berechne nun die Zwischenwerte der Geschwindigkeit und Energie
-        for j in (Ordnung+1):(l + Ordnung - 1)
+        for j in (ordnung+1):(l + ordnung - 1)
             # If-Schleife damit [j=3...N+1] eingehalten wird
             if sys.bound_cond == :reflective
-                if (j >= (Ordnung+2))
+                if (j >= (ordnung+2))
                     us[j] = zwischenwert_u(ρs_not_updated, ρs, us_copy, j, Δt, Δx)
                 end
                 ϵs[j] = zwischenwert_ϵ(ρs_not_updated, ρs, us_copy, ϵs_not_updated, j, Δt, Δx) 
@@ -254,9 +254,9 @@ function solve_euler(sys::HyDySys, σ::Float64, a::Float64, t_ende::Float64)
         # Berechnung der Kräfte und Druckarbeit, hier werden u und ϵ geupdated
         # u → u^1 → u^{n+1}
         # ϵ → ϵ^1 → ϵ^{n+1}
-        for j in (Ordnung + 1):(l + Ordnung - 1)
+        for j in (ordnung + 1):(l + ordnung - 1)
             # If-Schleife damit [j=3...N+1] eingehalten wird
-            if j >= (Ordnung+2)
+            if j >= (ordnung+2)
                 us[j] = u_new(ρs, us, ϵs, j, Δt, Δx, γ)
             end
             ϵs[j] = ϵ_new(ρs, us, ϵs, j, Δt, Δx, γ)
@@ -266,9 +266,9 @@ function solve_euler(sys::HyDySys, σ::Float64, a::Float64, t_ende::Float64)
         #println("Update-Energie:", ϵs)
 
         # Entferne Giisterzellen
-        us = us[(Ordnung+1):(end-Ordnung)]
-        ρs = ρs[(Ordnung+1):(end-Ordnung)]
-        ϵs = ϵs[(Ordnung+1):(end-Ordnung)]
+        us = us[(ordnung+1):(end-ordnung)]
+        ρs = ρs[(ordnung+1):(end-ordnung)]
+        ϵs = ϵs[(ordnung+1):(end-ordnung)]
         
 
         # Update Zeitschritt
